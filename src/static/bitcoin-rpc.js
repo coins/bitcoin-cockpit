0e200654d4a5add0f848bf0561e7e23eb7efa4bc
robinlinus <RobinLinus@users.noreply.github.com>
2023-07-20 16:31:46 +0200

    Add config for testnet and signet

--- src/static/bitcoin-rpc.js
+++ src/static/bitcoin-rpc.js
@@ -1,26 +1,32 @@
-async function bitcoinRPC(method, params = [], wallet = '') {
-    // Endpoint
-    const endpointUrl = `http://localhost:8332/${wallet}`
+// Endpoint
+const ENDPOINT_MAINNET = 'http://localhost:8332'
+const ENDPOINT_TESTNET = 'http://localhost:18332'
+const ENDPOINT_SIGNET = 'http://localhost:38332'
+const ENDPOINT = ENDPOINT_SIGNET
 
-    // Credentials 
-    const username = 'robin'
-    const password = 'password'
+// Credentials 
+const username = 'robin'
+const password = 'password'
+
+async function bitcoinRPC(method, params = [], wallet = '') {
+    // Compose endpoint
+    const url = `${ENDPOINT}/${wallet}`
 
     // Compose headers
     const authString = `${username}:${password}`
     const headers = { 'Authorization': 'Basic ' + btoa(authString) }
 
     // Compose body
-    let body = { id: 't0', 'method': method, 'params': params }
+    let body = { id: 't0', method: method, params: params}
     body = JSON.stringify(body)
 
     // Make request
     let response
     try {
-        response = await fetch(endpointUrl, { method: 'POST', headers, body })
+        response = await fetch(url, { method: 'POST', headers, body })
     } catch (error) {
         // Handle fetch error
-        throw `Failed to execute bitcoin RPC.\nMake sure bitcoind is running on ${endpointUrl}`
+        throw `Failed to execute bitcoin RPC.\nMake sure bitcoind is running on ${url}`
     }
 
     const responseJSON = await response.json()
@@ -30,5 +36,4 @@ async function bitcoinRPC(method, params = [], wallet = '') {
         return responseJSON.result
     else
         throw responseJSON.error.message
-
 }
